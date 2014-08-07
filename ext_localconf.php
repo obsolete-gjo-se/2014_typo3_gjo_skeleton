7f5c02b57e890da903db47c9bc561ce40b0f6c93
Gregory Jo <gregor.erdmann@emtec.de>
2014-08-07 11:27:51 +0200

    [CLEANUP] - Änderung PluginName auf SkeletonPlugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Gjo.' . $_EXTKEY,
-    'Skeleton',
+    'SkeletonPlugin',
     array(
         'Skeleton' => 'findAll, show, addForm, add, updateForm, update, remove'
     ),
--- ext_tables.php
+++ ext_tables.php
@@ -3,13 +3,6 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Gjo.' . $_EXTKEY,
-    'Skeleton',
-    'Skeleton Default'
+    'SkeletonPlugin',
+    'SkeletonPlugin Default'
 );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/Static',
-    'Skeleton'
-);
-
